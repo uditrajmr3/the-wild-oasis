3485c4d0390594275d8cb8cc3c577c6b411b4382
uditrajmr3 <csense2653@gmail.com>
2025-02-27 13:49:25 +0530

    --added: global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  background-color: #fff;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Welcome to The Wild Oasis</Heading>
+
+            <div>
+              <Heading as="h2">Check in & out</Heading>
+              <Button>Check In</Button>
+              <Button variation="secondary">Check Out</Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Forms</Heading>
+            <form>
+              <Input type="text" placeholder="Number of guests" />
+              <Input type="text" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
